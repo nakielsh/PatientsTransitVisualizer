05de9e2911e9919b8569b78734f1e2c8736044a7
mjakubowska <61580377+mjakubowska@users.noreply.github.com>
2020-12-31 01:00:48 +0100

    add reading file with Patients to GUI, adding prototype of constructor, getters and setters to Simulator class

--- Code/src/main/java/ptv/models/simulation/Simulator.java
+++ Code/src/main/java/ptv/models/simulation/Simulator.java
@@ -3,6 +3,7 @@ package ptv.models.simulation;
 import ptv.models.data.Country;
 import ptv.models.data.Patient;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Simulator {
@@ -10,6 +11,10 @@ public class Simulator {
     private boolean firstStep;
     private Country country;
     private List<Patient> patients;
+
+    public Simulator() {
+        patients = new ArrayList<>();
+    }
 
     public void makeStep(){
         throw new UnsupportedOperationException("Not implemented yet");
@@ -26,4 +31,18 @@ public class Simulator {
     private void nextStep(){
 
     }
+
+    public Country getCountry(){return this.country;}
+
+    public List<Patient> getPatients(){return this.patients;}
+
+    public void setCountry(Country country) {this.country = country;}
+
+    public void setPatients(List<Patient> patients){this.patients = patients;}
+
+    public void addPatient(Patient patient){this.patients.add(patient);}
+
+
+
+
 }
